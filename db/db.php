72a83ffad599e6615956334b8aebac0935754ac6
JanixPc <112666911+itsjanix@users.noreply.github.com>
2024-06-04 00:24:33 +0800

    Update db.php

--- db/db.php
+++ db/db.php
@@ -1,8 +1,8 @@
 <?php
-$host = 'localhost';
-$username = 'root';
-$password = '';
-$dbname = 'chatboxdb'; // Define the database name
+$host = 'sql211.infinityfree.com';
+$username = 'if0_36666529';
+$password = 'VLlfgQEOYT';
+$dbname = 'if0_36666529_chatboxdb';
 
 // Create a MySQLi connection
 $conn = new mysqli($host, $username, $password, $dbname);
